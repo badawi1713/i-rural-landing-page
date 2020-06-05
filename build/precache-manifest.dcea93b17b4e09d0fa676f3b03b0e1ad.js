6f4505179b661aa7f7985eb63f030dfd9167e3b5
badawi1713 <dzaky.badawi@gmail.com>
2020-06-05 09:25:30 +0700

    add build directory

---
+++ build/precache-manifest.dcea93b17b4e09d0fa676f3b03b0e1ad.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ac6e9267abc1e2557798a111a87c18b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24ddce164ff5900ea799",
+    "url": "/static/css/main.5f9e607d.chunk.css"
+  },
+  {
+    "revision": "5c7782049bcf7c05f1ff",
+    "url": "/static/js/2.24b06fb2.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.24b06fb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24ddce164ff5900ea799",
+    "url": "/static/js/main.c1ffd77f.chunk.js"
+  },
+  {
+    "revision": "20910ea712170fda7a27",
+    "url": "/static/js/runtime-main.febd39c3.js"
+  },
+  {
+    "revision": "2f0b3d2f9446b722ef0b84b926aac6e9",
+    "url": "/static/media/arrow-icon.2f0b3d2f.svg"
+  },
+  {
+    "revision": "3f4fc7fa905109e525cb70957a2e9bed",
+    "url": "/static/media/contact-background.3f4fc7fa.svg"
+  },
+  {
+    "revision": "d97727c6c2d99e74c253db11fb6298a8",
+    "url": "/static/media/facebook-logo.d97727c6.svg"
+  },
+  {
+    "revision": "fe438ee9e8572883ea579c25aa4d2199",
+    "url": "/static/media/gps-icon.fe438ee9.svg"
+  },
+  {
+    "revision": "854cb143084eb621d9f409b3f9902225",
+    "url": "/static/media/hero-background.854cb143.png"
+  },
+  {
+    "revision": "8c0b9e067bd19aa89a4c8096f85c06cc",
+    "url": "/static/media/i-rural-config.8c0b9e06.jpg"
+  },
+  {
+    "revision": "694b8131625ffb470de679159ebffe10",
+    "url": "/static/media/instagram-logo.694b8131.svg"
+  },
+  {
+    "revision": "5dcf0ac1b5bafa73e06262a679bfdd4d",
+    "url": "/static/media/irural-logo.5dcf0ac1.jpeg"
+  },
+  {
+    "revision": "4bb9878f64bf05052a1d3587c27ecafd",
+    "url": "/static/media/linkedin-logo.4bb9878f.svg"
+  },
+  {
+    "revision": "1d4f095086bc6001b0aa16b05cc5131a",
+    "url": "/static/media/router-icon.1d4f0950.svg"
+  },
+  {
+    "revision": "0e2cc935b63d6194cf1a159a288f0b0d",
+    "url": "/static/media/small-contact-background.0e2cc935.svg"
+  },
+  {
+    "revision": "8f04196074dbf3f1ad56befa92f506c4",
+    "url": "/static/media/small-hero-background.8f041960.png"
+  },
+  {
+    "revision": "fdc50e9d651ee76e1bec7d9a83740ff4",
+    "url": "/static/media/social-illustration.fdc50e9d.svg"
+  },
+  {
+    "revision": "38ece7b591a9ba07e199cddd8253312d",
+    "url": "/static/media/twitter-logo.38ece7b5.svg"
+  },
+  {
+    "revision": "b117265f98df504023743a90e899ee96",
+    "url": "/static/media/waiting-illustration.b117265f.svg"
+  },
+  {
+    "revision": "75574f55c43fa24fb6546a0607ee9e45",
+    "url": "/static/media/whatsapp-logo.75574f55.svg"
+  },
+  {
+    "revision": "62544e8a38c02926e9f78452779c6a2c",
+    "url": "/static/media/wifi-illustration.62544e8a.svg"
+  }
+]);
